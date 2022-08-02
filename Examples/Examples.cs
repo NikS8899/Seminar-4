c79c3353f2115ae138ebc882cf967346346cafe9
NikitaS <sergio791309@gmail.com>
2022-08-02 21:43:06 +0300

    Examples for seminar 4

---
+++ Examples/Examples.cs
@@ -0,0 +1,28 @@
+﻿// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+
+// for (int i = 1; i <= number; i++)
+// {
+//     sum = sum + i;
+// }
+// Console.WriteLine($"Искомая сумма: {sum}");
+
+// int[] array = GetBinaryArray(8);
+// Console.Write($"[{String.Join(",", array)}]");
+// int[] GetBinaryArray(int size)
+// {
+//     int[] result = new int[size];
+//     // new int[size] - массив размером size элементов
+//     // заполнен массив "0"
+//     // result.Length = size
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         result[i] = new Random().Next(2);// [0;2)
+//         // (2) - считаем, что числа от 0 до 1 (2 не включается)
+//     }
+//     return result; // int [] result
+// }
+
+
