65f423361705c6643c0a2745fcf54adabcf7f7f3
NikitaS <sergio791309@gmail.com>
2022-08-10 23:09:31 +0300

    Create task 2 seminar 4

---
+++ Task 2/Task 2.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (number > 0) 
+{
+    result += number % 10;
+    number /= 10;
+}
+System.Console.WriteLine($"Результат сложения всех цифр в числе = {result}");
