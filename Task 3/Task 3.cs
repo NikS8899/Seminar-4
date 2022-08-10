a38ae6ac76997407e1ac6ccccbb532ca8bb6a06f
NikitaS <sergio791309@gmail.com>
2022-08-10 23:15:22 +0300

    Create task 3 seminar 4

---
+++ Task 3/Task 3.cs
@@ -0,0 +1,12 @@
+﻿
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
+int[] array = GetArray(8);
+Console.Write($"[{String.Join(", ", array)}]");
