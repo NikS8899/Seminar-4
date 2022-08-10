2a88c0ef4d7995a8e31e350cd9b514d609a814d1
NikitaS <sergio791309@gmail.com>
2022-08-10 22:08:40 +0300

    Create task 1 seminar 4

---
+++ Examples/Task 1/Task 1.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+int result = Convert.ToInt32(Math.Pow (number, deg));
+System.Console.WriteLine($"Результат: {result} ");
+
